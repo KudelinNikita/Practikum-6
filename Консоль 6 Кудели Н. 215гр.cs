5d0f63698df38b298275e26de3a46d590959b8d7
KudelinNikita <Rokotanskiy8@gmail.com>
2024-11-19 13:24:52 +0200

    Add files via upload

---
+++ Консоль 6 Кудели Н. 215гр.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1.Используя двузначное случайное число, вывести на экран информацию о
+            //четности или нечетности этого числа с использованием тернарной операции.
+
+            //  Random rnd = new Random();
+            //int chislo = rnd.Next(10, 99);
+            //Console.WriteLine($"Случайное число={chislo}");
+            //string result = chislo % 2 == 0 ? "Чётное":"нечётное";
+            //Console.WriteLine($"Число{chislo}-{result}");
+            //Console.ReadKey();
+
+            //Задача 2.Дано целое число n, указывающее номер дня недели от 1 до 7.По указанному номеру n вывести название соответствующего дня недели
+           
+            //    Console.Write("Введите номер дня недели (от 1 до 7):");
+            //    int n = int.Parse(Console.ReadLine());
+
+            //    switch (n)
+            //    {
+            //        case 1:
+            //            Console.WriteLine("Понедельник");
+            //            break;
+            //        case 2:
+            //            Console.WriteLine("Вторник");
+            //            break;
+            //        case 3:
+            //            Console.WriteLine("Среда");
+            //            break;
+            //        case 4:
+            //            Console.WriteLine("Четверг");
+            //            break;
+            //        case 5:
+            //            Console.WriteLine("Пятница");
+            //            break;
+            //        case 6:
+            //            Console.WriteLine("Суббота");
+            //            break;
+            //        case 7:
+            //            Console.WriteLine("Воскресенье");
+            //            break;
+            //        default: Console.WriteLine("Неверный номер дня недели");
+            //            break;
+            //    }
+
+            
+            
+            
+            //Console.ReadLine();
+        }
+    }
+}
